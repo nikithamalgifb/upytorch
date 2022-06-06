489f78f5b8ca5c87c61f28c9fed90d4548ec7cf0
Rohan Ahluwaila <rohanahluwalia@fb.com>
2022-06-06 16:51:34 -0700

    Fixed Interpreter Manager Class

--- examples/interpreterManager.cpp
+++ examples/interpreterManager.cpp
@@ -2,11 +2,11 @@
 
 Interpreter::Interpreter(int nInterp){
     char heap[16384];
-    // // Initialized stack limit
-    // mp_stack_set_limit(40000 * (BYTES_PER_WORD / 4));
-    // // Initialize heap
-    // gc_init(heap, heap + sizeof(heap));
-    // // Initialize interpreter
-    // mp_init();
+    // Initialized stack limit
+    mp_stack_set_limit(40000 * (BYTES_PER_WORD / 4));
+    // Initialize heap
+    gc_init(heap, heap + sizeof(heap));
+    // Initialize interpreter
+    mp_init();
 
 }
