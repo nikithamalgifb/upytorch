733ebe2a1d41d6222dfbb97f7ef85a08a9949c5b
Rohan Ahluwaila <rohanahluwalia@fb.com>
2022-06-06 17:22:47 -0700

    Simple TorchScript Model added

---
+++ torch-models/simple-model.py
@@ -0,0 +1,16 @@
+import torch
+
+
+# Define a Simple Model class (Only using floats)
+class SimpleModel(torch.nn.Module):
+    def __init__(self):
+        super(SimpleModel, self).__init__()
+
+    def forward(self, x: float):
+        new_x = x ** 2
+        return new_x
+
+
+# Save the model as .pt file
+model = torch.jit.script(SimpleModel())
+torch.jit.save(model, "simple-model.pt")
